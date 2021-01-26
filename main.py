8a7b666b2fb13b223d3f507238f99f6a3b6a9184
psatala <psatala@example.com>
2021-01-27 00:32:29 +0100

    Add charts

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 from input import input
 from bfs import bfs
 from aco import aco
-from constants import BFS
+from constants import BFS, N_EPOCHS, N_RUNS
+from plot import createSummary
+
+import numpy as np
+import random
+import time
 
 def main():
+
+    #seed
+    seed = int(round(time.time()))
+    np.random.seed(seed)
+
+
     #read from file
     filepath = "nobel-eu.txt"
     nodes = input(filepath)
 
-    #BFS algorithm
-    distance = bfs(nodes, "Warsaw")
-    for key, value in distance.items():
-        print(str(key) + ": " + str(value))
-    
-    aco(nodes, "Warsaw", "London", BFS)
+
+
+    minPathLength = []
+    averagePathLength = []
+    maxPathLength = []
+    optimalSolution = []
+
+    for i in range(N_RUNS):
+
+        #select start and end at random (must be different)
+        startNodeName = random.choice(list(nodes.keys()))
+        endNodeName = startNodeName
+        while startNodeName == endNodeName:
+            endNodeName = random.choice(list(nodes.keys()))
+
+        #print info about selected nodes
+        print(i, startNodeName, endNodeName)
+
+        #calculate optimal
+        distance = bfs(nodes, startNodeName)
+        optimalSolution.append(distance[endNodeName])
+
+        #calculate aco
+        currentMinPathLength, currentAveragePathLength, currentMaxPathLength = aco(nodes, startNodeName, endNodeName, BFS)
+        
+        #add to final results
+        minPathLength.append(currentMinPathLength)
+        averagePathLength.append(currentAveragePathLength)
+        maxPathLength.append(currentMaxPathLength)
+
+    #draw plot
+    createSummary(minPathLength, averagePathLength, maxPathLength, optimalSolution, seed, BFS)
+
 
 
 
